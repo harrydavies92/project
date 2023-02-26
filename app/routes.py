24ab7e6a6a67cb892e17ab05d597281a8016d2f6
harrydavies92 <ed18h5d@leeds.ac.uk>
2023-02-26 16:00:05 +0000

    Homepage added. Refresh and save buttons added. Tables now editable.

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,91 @@
 import pandas as pd
 
 from app import app, db
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from .models import Student, Staff, Project
 from .stakeholderFunctions import staffForCode,normaliseCode
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route("/students")
 def students():
-    Student.query.delete()
-    df = pd.read_csv('datafiles/anonymisedForms.csv')
-    for i, row in df.iterrows():
-        student = Student(
-            name = row[4],
-            username = row[3].split('@')[0],
-            degree = row[7],
-            code1 = normaliseCode(row[9]),
-            title1 = row[10],
-            reason1 = row[11],
-            code2 = normaliseCode(row[12]),
-            title2 = row[13],
-            reason2 = row[14],
-            code3 = normaliseCode(row[15]),
-            title3 = row[16],
-            reason3 = row[17],
-            code4 = normaliseCode(row[18]),
-            title4 = row[19],
-            reason4 = row[20],
-            allocated_code = '0',
-            allocated_staff = "blank"
-        )
-        db.session.add(student)
-    db.session.commit()
-
     students = Student.query.all()
     return render_template('studentTable.html', students=students)
 
+@app.route('/students/update', methods=['POST'])
+def update_students():
+    data = request.get_json()
+    for row in data:
+        student = Student.query.filter_by(id=row['id']).first()
+        student.name = row['name']
+        student.username = row['username']
+        student.degree = row['degree']
+        student.code1 = row['code1']
+        student.title1 = row['title1']
+        student.reason1 = row['reason1']
+        student.code2 = row['code2']
+        student.title2 = row['title2']
+        student.reason2 = row['reason2']
+        student.code3 = row['code3']
+        student.title3 = row['title3']
+        student.reason3 = row['reason3']
+        student.code4 = row['code4']
+        student.title4 = row['title4']
+        student.reason4 = row['reason4']
+        student.allocated_code = row['allocated_code']
+        student.allocated_staff = row['allocated_staff']
+        db.session.commit()
+    return 'Data updated successfully!'
+
+@app.route('/students/refresh')
+def refresh_students():
+    df = pd.read_csv('datafiles/anonymisedForms.csv')
+    Student.query.delete()
+    for i, row in df.iterrows():
+        student = Student(
+            name=row[4],
+            username=row[3].split('@')[0],
+            degree=row[7],
+            code1=normaliseCode(row[9]),
+            title1=row[10],
+            reason1=row[11],
+            code2=normaliseCode(row[12]),
+            title2=row[13],
+            reason2=row[14],
+            code3=normaliseCode(row[15]),
+            title3=row[16],
+            reason3=row[17],
+            code4=normaliseCode(row[18]),
+            title4=row[19],
+            reason4=row[20],
+            allocated_code='0',
+            allocated_staff='blank'
+        )
+        db.session.add(student)
+    db.session.commit()
+    students = Student.query.all()
+    return redirect(url_for('students'))
+
 @app.route("/staff")
 def staff():
+    staff = Staff.query.all()
+    return render_template('staffTable.html', staff=staff)
+
+@app.route('/staff/update', methods=['POST'])
+def update_staff():
+    data = request.get_json()
+    for row in data:
+        staff = Staff.query.filter_by(id=row['id']).first()
+        staff.name = row['name']
+        staff.max_load = row['max_load']
+        staff.current_load = row['current_load']
+        db.session.commit()
+    return 'Data updated successfully!'
+
+@app.route("/staff/refresh")
+def refresh_staff():
     Staff.query.delete()
 
     all_codes = set()
@@ -54,11 +103,29 @@ def staff():
             db.session.add(staff_member)
             db.session.commit()
 
-    staff = Staff.query.all()
-    return render_template('staffTable.html', staff=staff)
+    return redirect(url_for('staff'))
 
-@app.route("/project")
-def project():
+@app.route("/projects")
+def projects():
+    projects = Project.query.all()
+    return render_template('projectTable.html', projects=projects)
+
+@app.route('/projects/update', methods=['POST'])
+def update_projects():
+    data = request.get_json()
+    for row in data:
+        projects = Project.query.filter_by(id=row['id']).first()
+        projects.code = row['code']
+        projects.title = row['title']
+        projects.staff_member = row['staff_member']
+        projects.max_load = row['max_load']
+        projects.current_load = row['current_load']
+        projects.popularity = row['popularity']
+        db.session.commit()
+    return 'Data updated successfully!'
+
+@app.route("/projects/refresh")
+def refresh_projects():
     # Delete all records in the Project table
     Project.query.delete()
 
@@ -84,7 +151,4 @@ def project():
 
     # Commit changes to the database
     db.session.commit()
-
-    # Query all Project records and pass them to the projectTable.html template for rendering
-    projects = Project.query.order_by(Project.title).all()
-    return render_template('projectTable.html', projects=projects)
+    return redirect(url_for('projects'))
